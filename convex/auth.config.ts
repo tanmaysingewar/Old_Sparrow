6d80e6304a1c7980f22a2ea85bb558c5a565b416
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2025-06-28 17:18:09 +0530

    auth url fix

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,12 +1,21 @@
+// export default {
+//   providers: [
+//     {
+//       domain: "https://successful-trout-122.convex.site",
+//       applicationID: "convex",
+//     },
+//     {
+//       domain: "https://diligent-schnauzer-735.convex.site",
+//       applicationID: "convex_production",
+//     },
+//   ],
+// };
+
 export default {
   providers: [
     {
-      domain: "https://successful-trout-122.convex.site",
+      domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
-    },
-    {
-      domain: "https://diligent-schnauzer-735.convex.site",
-      applicationID: "convex_production",
     },
   ],
 };
